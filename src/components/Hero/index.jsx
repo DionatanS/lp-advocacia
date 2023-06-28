713bf8ce4106bec2889a4c410c3127586b5d096a
Dionatan da Silva <dionatans@gazetadopovo.com.br>
2023-06-28 17:42:24 -0300

    teste

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -8,7 +8,7 @@ export default function Hero({children}){
                 <img src="/images/logo.png" alt="" className={styles.imgLogo}/>
                 <div className={styles.cointainerHeroText}>
                     <h2 className={styles.subtituloHero}>Não aceite benefícios <span className={styles.destaque}>negados</span> ou em <span className={styles.destaque}>análise</span> por tempo indefinido!</h2>
-                    <h3 className={styles.textHero}><span className={styles.bold}>Temos a Solução para seus Benefícios Previdenciários!</span> Atendemos em todo o Brasil com Consultoria Especializada em <span className={styles.destaque}>Previdência Social</span>. Não deixe seus direitos serem negados, estamos aqui para oferecer a solução que você precisa.</h3>
+                    <p className={styles.textHero}><span className={styles.bold}>Temos a Solução para seus Benefícios Previdenciários!</span> Atendemos em todo o Brasil com Consultoria Especializada em <span className={styles.destaque}>Previdência Social</span>. Não deixe seus direitos serem negados, estamos aqui para oferecer a solução que você precisa.</p>
                     <a href="" className={styles.buttonFaleConosco}>FALE CONOSCO <img src="/images/iconeWhatsApp.png" alt="" /></a>
                 </div>
             </div>
