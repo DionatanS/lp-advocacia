1e456b515b339dba02b5bb62d9386c0c68528b11
Dionatan da Silva <dionatans@gazetadopovo.com.br>
2023-06-29 17:44:01 -0300

    Ajuste na font para ganho de performance

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,20 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang='pt-BR'>
         <Head>
-        <meta name="robots" content="all"></meta>
-        <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
-        <meta name="google" content="notranslate" key="notranslate" />       
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com"  />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet"/>
+          <meta name="robots" content="all" />
+          <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+          <meta name="google" content="notranslate" key="notranslate" />       
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com"  />
         </Head>
         <body>
           <Main />
           <NextScript />
+          <Script src="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" strategy="beforeInteractive" />
         </body>
       </Html>
     );
