63293c1cb41f959318e7ccab9fb502f45ba41220
Dionatan da Silva <dionatans@gazetadopovo.com.br>
2023-07-03 09:52:52 -0300

    Ajuste SEO

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,8 +7,8 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 <title>Advogado previdenciario</title>
                 <meta name="robots" content="all" />
-                <meta name="description" content="Agafarma - Sua farmácia de confiança para atender às suas necessidades de saúde e bem-estar. Oferecemos uma ampla variedade de medicamentos, produtos de cuidados pessoais e serviços farmacêuticos de qualidade. Nossa equipe dedicada de profissionais farmacêuticos está pronta para fornecer orientação especializada e atendimento personalizado. Conte com a Agafarma para encontrar tudo o que você precisa para cuidar da sua saúde e da sua família. Visite-nos hoje mesmo e experimente o atendimento excepcional que nos tornou uma referência na comunidade." />
-                <meta name="keywords" content="farmácia, agafarma, remedios, medicamentos, genéricos, antibióticos, anti-alérgico, xarope, anticoncepcional,fraldas, manipulados, popular, promoção, comprimido, contato, capsula"></meta>
+                <meta name="description" content="AposentaINSS.com - Seu parceiro em advocacia previdenciária e muito mais. Nossos advogados especializados em direito previdenciário estão prontos para ajudar você a garantir seus direitos. Além disso, oferecemos serviços jurídicos abrangentes em diversas áreas do direito. Entre em contato conosco para uma consultoria personalizada e confiável." />
+                <meta name="keywords" content="advogados, advocacia previdenciária, direito previdenciário, advocacia, consultoria jurídica, aposentadoria, INSS, direito trabalhista, direito civil, direito de família, direito criminal, direito tributário, direito empresarial, direito do consumidor, direito imobiliário, direito administrativo, direito ambiental, direito internacional, experiência de 10 anos, advogados especializados, assessoria jurídica, consultoria legal, representação legal"></meta>
                 <meta name="author" content="Agafarma"/>
                 <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
                 <meta name="google" content="notranslate" key="notranslate" />
